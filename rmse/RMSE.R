219beb5bcb8ff8ee9633a28ca2d6471bd015bb14
JoannaPokora <pokorajo@gmail.com>
2026-01-31 01:38:48 +0100

    fixed rmse

--- rmse/RMSE.R
+++ rmse/RMSE.R
@@ -23,17 +23,17 @@ rmse_s1_df <- data.frame(
   rmse_fit = c(
     with_imp_summary_df %>% filter(model == "sarima_own") %>%pull(rmse_fit),
     apply(sarima_models_fit$`bez epidemii`[1:length(true_vals_train), ], 2, rmse, true_vals_train),
-    rmse(exp(forecast_without_cov_auto[1:length(true_vals_train), "yhat"]), true_vals_train)
+    rmse(forecast_without_cov_auto[1:length(true_vals_train), "yhat"], true_vals_train)
     ),
   rmse_covid = c(
     with_imp_summary_df %>% filter(model == "sarima_own") %>%pull(rmse_cov),
     apply(sarima_models_fit$`bez epidemii`[(length(true_vals_train)+1):(N - length(true_vals_test)), ], 2, rmse, true_vals_cov),
-    rmse(exp(forecast_without_cov_auto[(length(true_vals_train)+1):(N - length(true_vals_test)), "yhat"]), true_vals_cov)
+    rmse(forecast_without_cov_auto[(length(true_vals_train)+1):(N - length(true_vals_test)), "yhat"], true_vals_cov)
   ),
   rmse_test = c(
     with_imp_summary_df %>% filter(model == "sarima_own") %>%pull(rmse_after_cov),
     apply(sarima_models_fit$`bez epidemii`[(N - length(true_vals_test) + 1):N, ], 2, rmse, true_vals_test),
-    rmse(exp(forecast_without_cov_auto[(N- length(true_vals_test) + 1):N, "yhat"]), true_vals_test)))
+    rmse(forecast_without_cov_auto[(N- length(true_vals_test) + 1):N, "yhat"], true_vals_test)))
 
 
 # Scenariusz 2:
@@ -48,34 +48,35 @@ rmse_s2_df <- data.frame(
   rmse_fit = c(
     rmse_table %>% filter(model_id == "211") %>% pull(rmse_fit),
     apply(sarima_models_fit$`z epidemią`[1:length(true_vals_train_with_cov), ], 2, rmse, true_vals_train_with_cov),
-    rmse(exp(forecast_with_cov_auto[1:length(true_vals_train_with_cov), "yhat"]), true_vals_train_with_cov),
+    rmse(forecast_with_cov_auto[1:length(true_vals_train_with_cov), "yhat"], true_vals_train_with_cov),
     rmse(forecast_with_cov[1:length(true_vals_train_with_cov), "yhat"], true_vals_train_with_cov)
     ),
   rmse_covid = matrix(NA, nrow = 5, ncol =1),
   rmse_test = c(
     rmse_table %>% filter(model_id == "211") %>% pull(rmse_pred),
     apply(sarima_models_fit$`z epidemią`[(length(true_vals_train_with_cov) + 1):N, ], 2, rmse, true_vals_test),
-    rmse(exp(forecast_with_cov_auto[(length(true_vals_train_with_cov) + 1):N, "yhat"]), true_vals_test),
+    rmse(forecast_with_cov_auto[(length(true_vals_train_with_cov) + 1):N, "yhat"], true_vals_test),
     rmse(forecast_with_cov[(length(true_vals_train_with_cov) + 1):N, "yhat"], true_vals_test)))
 
 
 # Scenariusz 3:
 
-forecast_imp_cov_auto <- predict(prophet_models$imputacja, data.frame(ds = model_data$time))
+forecast_imp_cov_auto <- predict(prophet_models$imputacja,
+                                 data.frame(ds = model_data$time))
 
 rmse_s3_df <- data.frame(
   model = c("sarima_111_011", "sarima_auto", "sarima_max", "prophet_auto"),
   scenario = rep(3, 4),
   rmse_fit = c(
-    rmse_table_imp %>% filter(model_id == "1101") %>% pull(rmse_imp_fit),
+    rmse_table_imp %>% filter(model_id == "1101") %>% pull(rmse_fit),
     apply(sarima_models_fit$imputacja[1:length(true_vals_train_with_cov), ], 2, rmse, true_vals_train_with_cov_imp),
-    rmse(exp(forecast_imp_cov_auto[1:length(true_vals_train_with_cov), "yhat"]), true_vals_train_with_cov_imp)
+    rmse(forecast_imp_cov_auto[1:length(true_vals_train_with_cov), "yhat"], true_vals_train_with_cov_imp)
     ),
   rmse_covid = matrix(NA, nrow = 4, ncol =1),
   rmse_test = c(
     rmse_table_imp %>% filter(model_id == "1101") %>% pull(rmse_pred),
     apply(sarima_models_fit$imputacja[(length(true_vals_train_with_cov) + 1):N, ], 2, rmse, true_vals_test),
-    rmse(exp(forecast_imp_cov_auto[(length(true_vals_train_with_cov) + 1):N, "yhat"]), true_vals_test)))
+    rmse(forecast_imp_cov_auto[(length(true_vals_train_with_cov) + 1):N, "yhat"], true_vals_test)))
 
 
 # Combine:
@@ -84,10 +85,10 @@ rmse_full_df <- rbind(rmse_s1_df, rmse_s2_df, rmse_s3_df)
 rmse_full_df
 
 # saveRDS(rmse_full_df, "rmse_results_df.RDS")
-readRDS("rmse_results_df.RDS")
+readRDS("rmse/rmse_results_df.RDS")
 
 rmse_full_df %>% filter(scenario == 1) %>% sort_by(~rmse_fit)
-rmse_full_df %>% filter(scenario == 1) %>% sort_by(~rmse_test)
+rmse_full_df %>% filter(scenario == 1) %>% sort_by(~rmse_covid)
 rmse_full_df %>% filter(scenario == 1) %>% sort_by(~rmse_test)
 
 rmse_full_df %>% filter(scenario != 1) %>% sort_by(~rmse_fit)
